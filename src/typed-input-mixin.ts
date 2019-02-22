95b4bc842ace4ef2bd4be265b21136417bb11dd2
Giorgio Garasto <giorgio@garasto.it>
2019-02-22 20:18:26 +0100

    feature: Implement a typed input mixin

---
+++ src/typed-input-mixin.ts
@@ -0,0 +1,39 @@
+type Constructor<T> = new(...args: any[]) => T;
+
+export const typed =
+  <T extends Constructor<HTMLInputElement>>(baseElement: T) =>
+    class extends baseElement {
+    // Note: we have to use the any type because otherwise TypeScript complains
+    set value(val: any) {
+      if (val instanceof Date) {
+        super.value = val.toISOString().slice(0, super.type === 'date' ? 10 : -1);
+        return;
+      }
+      if (typeof val === 'boolean') {
+        super.checked = val;
+        return;
+      }
+      super.value = `${val}`;
+    }
+
+    get value(): any {
+      switch (super.type) {
+        case 'checkbox':
+        case 'radio':
+          return super.checked;
+        case 'date':
+        case 'datetime-local':
+          return new Date(super.value);
+        case 'number':
+        case 'range':
+          return parseFloat(super.value);
+        // TODO: decide what to do with other input types (e.g. month, time, week, etc.)
+      }
+      // By default, just return the value attribute as a string
+      return super.value;
+    }
+
+    get rawValue(): string {
+      return super.value;
+    }
+  };
