b25061eb5a3529328c3104ac13a31851c474c256
Giorgio Garasto <giorgio@garasto.it>
2019-02-23 14:56:18 +0100

    refactor: Make typed input element use the new mixin

--- src/typed-input.ts
+++ src/typed-input.ts
@@ -1,37 +1,5 @@
-export class HTMLTypedInputElement extends HTMLInputElement {
-  // Note: we have to use the any type because otherwise TypeScript complains
-  set value(val: any) {
-    if (val instanceof Date) {
-      super.value = val.toISOString().slice(0, super.type === 'date' ? 10 : -1);
-      return;
-    }
-    if (typeof val === 'boolean') {
-      super.checked = val;
-      return;
-    }
-    super.value = `${val}`;
-  }
+import { typed } from './typed-input-mixin';
 
-  get value(): any {
-    switch (super.type) {
-      case 'checkbox':
-      case 'radio':
-        return super.checked;
-      case 'date':
-      case 'datetime-local':
-        return new Date(super.value);
-      case 'number':
-      case 'range':
-        return parseFloat(super.value);
-      // TODO: decide what to do with other input types (e.g. month, time, week, etc.)
-    }
-    // By default, just return the value attribute as a string
-    return super.value;
-  }
-
-  get rawValue(): string {
-    return super.value;
-  }
-}
+export class HTMLTypedInputElement extends typed(HTMLInputElement) {}
 
 customElements.define('typed-input', HTMLTypedInputElement, { extends: 'input' });
